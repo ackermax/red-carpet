1774be2792e52c67ae2a4e4e5d83e1397e2b4197
jasapper <jasap98@yahoo.com>
2018-01-26 23:42:49 -0500

    Adding Passport and refining User table

--- models/user.js
+++ models/user.js
@@ -1,8 +1,42 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-      // Giving the User model a name of type STRING
-      name: DataTypes.STRING
-    });
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      
+      firstname: {
+        type: DataTypes.STRING,
+        notEmpty: true
+      },
+
+      lastname: {
+        type: DataTypes.STRING,
+        notEmpty: true
+      },
+
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: true
+        }
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+
+      last_login: {
+        type: DataTypes.DATE
+      },
+
+      status: {
+        type: DataTypes.ENUM("active", "inactive"),
+        defaultValue: "active"
+      }
+      });
   
     User.associate = function(models) {
       // Associating User with Movies
--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs  = require('express-handlebars');
+var exphbs = require("express-handlebars");
+var passport = require("passport")
+var session = require("express-session")
+var env = require("dotenv").load();
 
 
 var app = express();
@@ -10,14 +13,19 @@ var PORT = process.env.PORT || 3000;
 
 var db = require("./models");
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars'); 
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars"); 
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
+
+// Passport authentication
+app.use(session({ secret: "codingbootcamp",resave: true, saveUninitialized:true}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login
 
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {

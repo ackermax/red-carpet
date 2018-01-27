a7165e43f199ed281c4c825ef56f34ee3efe4634
Steve Walker <captnwalker@gmail.com>
2018-01-27 17:57:19 -0500

    updates to all Views; hi-res splash background img

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs  = require('express-handlebars');
 
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
